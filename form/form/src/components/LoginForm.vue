73578fd5abbdcceb0fe482ba2aa29f3b08740e03
giddy77 <giddysipoi@gmail>
2024-02-04 13:22:40 +0300

    simple login form

---
+++ form/form/src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+<h1>This is the login form</h1>
+<input type="text" v-model="formdata.username" placeholder="enter name"><br><br>
+<input type="password" v-model="formdata.password" placeholder="enter password"><br><br>
+
+<button v-on:click="login">login</button>
+</template>
+<script>
+export default{
+    name: 'LoginForm',
+    data()
+    {
+        return{
+            formdata:{
+                username:null,
+                password:null
+            }
+}
+    },
+    methods:
+   {
+    login()
+    {
+        console.log('login is called')
+        console.log(this.formdata)
+    }
+   }
+
+}
+</script>

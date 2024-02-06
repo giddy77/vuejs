0d6fc7aab5735eba1bcf7550101dc9a22bb8aa4d
giddy77 <giddysipoi@gmail>
2024-02-06 12:48:50 +0300

    consumed starwars api using axios

---
+++ api/api/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <ConsumeApi />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ConsumeApi from './components/ConsumeApi'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    ConsumeApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ api/api/src/components/ConsumeApi.vue
@@ -0,0 +1,37 @@
+<template>
+<h1>Consume external api</h1>
+<table>
+    <tr>
+        <th>name</th>
+        <th>model</th>
+    </tr>
+    <tr v-for="item in starships" v-bind:key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.model }}</td>
+    </tr>
+</table>
+</template>
+<script>
+import axios from 'axios';
+export default{
+    name: 'ConsumeApi',
+    data(){
+        return{
+            data:undefined,
+            starships:[]
+        }
+    },
+    mounted()
+    {
+         
+            axios.get('https://swapi.dev/api/starships')
+            .then((response)=>{
+                this.data = response.data.results
+                
+                this.starships = this.data.map(item => item)
+                console.log(response.data.results);
+            })
+        
+    }
+}
+</script>
